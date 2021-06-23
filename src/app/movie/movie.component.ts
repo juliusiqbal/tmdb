2400572ced3dc2787a343777b7f1dcca78b30ec3
Julius Iqbal <julius.jsr@gmail.com>
2021-06-24 05:12:27 +0600

    TMDB-2: Increased asset budget. Added columns, column headers and data schema.

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -8,7 +8,76 @@ import { MovieService } from './movie.service';
   templateUrl: './movie.component.html',
 })
 export class MovieComponent implements OnInit {
-  public data: any[] = [];
+  public colHeaders: string[] = [
+    'Budget',
+    'Genres',
+    'Homepage',
+    'Id',
+    'Keywords',
+    'Original Language',
+    'Original Title',
+    'Overview',
+    'Popularity',
+    'Production Companies',
+    'Production Countries',
+    'Release Date',
+    'Revenue',
+    'Runtime',
+    'Spoken Languages',
+    'Status',
+    'Tagline',
+    'Title',
+    'Vote Average',
+    'Vote Count',
+  ];
+
+  public columns: object[] = [
+    { data: 'budget' },
+    { data: 'genres' },
+    { data: 'homepage' },
+    { data: 'id' },
+    { data: 'keywords' },
+    { data: 'original_language' },
+    { data: 'original_title' },
+    { data: 'overview' },
+    { data: 'popularity' },
+    { data: 'production_companies' },
+    { data: 'production_countries' },
+    { data: 'release_date' },
+    { data: 'revenue' },
+    { data: 'runtime' },
+    { data: 'spoken_languages' },
+    { data: 'status' },
+    { data: 'tagline' },
+    { data: 'title' },
+    { data: 'vote_average' },
+    { data: 'vote_count' },
+  ];
+
+  public data: object[] = [];
+
+  public dataSchema: object = {
+    budget: null,
+    genres: null,
+    homepage: null,
+    id: null,
+    keywords: null,
+    original_language: null,
+    original_title: null,
+    overview: null,
+    popularity: null,
+    production_companies: null,
+    production_countries: null,
+    release_date: null,
+    revenue: null,
+    runtime: null,
+    spoken_languages: null,
+    status: null,
+    tagline: null,
+    title: null,
+    vote_average: null,
+    vote_count: null,
+  };
 
   constructor(private movieService: MovieService) {}
 
